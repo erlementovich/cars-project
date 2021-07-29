96f05917309b8a1105350e710525956dd4e217c6
artem <artem@startted.com>
2021-07-29 10:58:27 +0300

    185826 / Поправил страницу аккаунта

--- resources/views/pages/account/index.blade.php
+++ resources/views/pages/account/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
-@section('title', 'Аккаунт')
+@section('title', 'Страница аккаунта')
 
 @section('content')
-    <div class="p-4">
-        <h1 class="text-black text-3xl font-bold mb-4">Пример отображение шапки для авторизованного пользователя</h1>
-    </div>
+    <p>Контент аккаунта</p>
 @endsection
