0b49ea3cc243790fd9bcd3238b3e88d513049293
artem <artem@startted.com>
2021-08-04 19:35:11 +0300

    185836 / Добавил events

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use App\Events\CarCreatedEvent;
 use App\Events\CarDeletedEvent;
 use App\Events\CarUpdatedEvent;
+use App\Events\ModelCreatedEvent;
+use App\Events\ModelDeletedEvent;
+use App\Events\ModelUpdatedEvent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,18 +16,18 @@ class Car extends Model
     use HasFactory;
 
     protected $dispatchesEvents = [
-        'created' => CarCreatedEvent::class,
-        'updated' => CarUpdatedEvent::class,
-        'deleted' => CarDeletedEvent::class,
+        'created' => ModelCreatedEvent::class,
+        'updated' => ModelUpdatedEvent::class,
+        'deleted' => ModelDeletedEvent::class,
     ];
 
     protected $fillable = [
-      'name',
-      'body',
-      'price',
-      'price',
-      'old_price',
-      'car_body_id'
+        'name',
+        'body',
+        'price',
+        'price',
+        'old_price',
+        'car_body_id'
     ];
 
     public function carEngine()
